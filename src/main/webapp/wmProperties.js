721a942db7eec813036e796f4ac0bc2d489c2da1
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2018-03-13 13:10:01 +0000

    updated source New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,hrdb,fonts,passion,en.json,passion] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,lib,logos,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,build.xml,pom.xml,README.md,Readme.txt,Readme.txt,README.md] Removed files - [fonts,material]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "passion",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestsharePrj13mar",
+  "homePage" : "Main",
+  "name" : "TestsharePrj13mar",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
